653dfad2f7fb051acb3511140235440bec123375
Lubos Zima <lzima@cmgx.io>
2024-06-25 19:09:28 +0700

    fix: issue with current session dont have in path wget

--- template/.chezmoiscripts/darwin/run_onchange_after_05-company-portal.sh
+++ template/.chezmoiscripts/darwin/run_onchange_after_05-company-portal.sh
@@ -4,7 +4,7 @@
 if ! [ -d "/Applications/Company Portal.app" ]; then
   echo "Company Portal wasn't found on your computer"
   echo "Downloading Company Portal from Microsoft"
-  wget -O /tmp/CompanyPortal.pkg https://go.microsoft.com/fwlink/?linkid=853070
+  bash -c 'wget -O /tmp/CompanyPortal.pkg https://go.microsoft.com/fwlink/?linkid=853070'
 
   echo "Installing Company Portal, you will be asked for password:"
   sudo installer -verbose -pkg /tmp/CompanyPortal.pkg -target /
