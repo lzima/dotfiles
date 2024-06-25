7cc75f86f76811a052516bbaeb8460d616d8f458
Lubos Zima <lzima@cmgx.io>
2024-06-25 18:12:42 +0700

    feat: add installation script

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+function usage() {
+  echo "Usage: $0 <email> <first-name> <family-name> <github-username>"
+}
+
+# help if -h param
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    usage
+    exit 0;
+fi
+
+if ! [ -z "$1" ]; then
+  CHZ_EMAIL="$1"
+fi
+
+if ! [ -z "$2" ]; then
+  CHZ_FIRSTNAME="$2"
+fi
+
+if ! [ -z "$3" ]; then
+  CHZ_FAMILYNAME="$3"
+fi
+
+if ! [ -z "$4" ]; then
+  CHZ_GITHUBUSERNAME="$4"
+fi
+
+prompt=""
+if ! [ -z "$CHZ_EMAIL" ]; then
+  prompt="$prompt --promptString email=$CHZ_EMAIL"
+fi
+
+if ! [ -z "$CHZ_FIRSTNAME" ]; then
+  prompt="$prompt --promptString firstName=$CHZ_FIRSTNAME"
+fi
+
+if ! [ -z "$CHZ_FAMILYNAME" ]; then
+  prompt="$prompt --promptString familyName=$CHZ_FAMILYNAME"
+fi
+
+if ! [ -z "$CHZ_GITHUBUSERNAME" ]; then
+  prompt="$prompt --promptString githubUsername=$CHZ_GITHUBUSERNAME"
+fi
+
+sh -c "$(curl -fsLS https://git.io/chezmoi)" -- -b ~/.local/bin \
+init --one-shot --force --apply $prompt lzima
